8b81a245e7c2088e94d056f6a293a1d6b47e708d
BratHuhn <m.schaefer@machit.de>
2019-10-21 20:54:24 +0200

    added while

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 
 import routingModule
 
@@ -13,7 +14,7 @@ class pairings:
         self.filedir = './pairings/'
         self.filedirtemp = './pairings/temp/'
         p = Path('pairings/temp')
-        p.mkdir(exist_ok=True)
+        p.mkdir(exist_ok=True, parents=True)
 
     def fetchMasterData(self):
         try:
@@ -34,8 +35,8 @@ class pairings:
             print(f'HTTP error occurred: {http_err}')  # Python 3.6
         except Exception as err:
             print(f'Other error occurred: {err}')  # Python 3.6
-        else:
-            print('Success getting pairs!')
+        #else:
+            #print('Success getting pairs!')
 
     def splitMasterdata(self, currency):
         f = open(self.filedir + "pairing.json", "r")
@@ -77,8 +78,10 @@ class pairings:
         f = open(self.filedir + compareCurrency + startCurrency + 'Circuit.json', "r")
         circuits = json.loads(f.read())
         precision = 'P0'
+        counter = 0
         for circuit in circuits:
             i = 0
+            
             for pair in circuit:
                 # Check if pair orderbook already fetched
                 currentPair = Path(self.filedirtemp + pair + '.json')
@@ -91,24 +94,31 @@ class pairings:
                         if(i > 0):
                             # seller
                             for order in jsonOrderbook:
-                                if order[2] < 0:
+                                if order[2] > 0:
                                     jsonOrderbookFiltered.append(order)
                         else:
                             # buyer
                             for order in jsonOrderbook:
-                                if order[2] > 0:
+                                if order[2] < 0:
                                     jsonOrderbookFiltered.append(order)
                         with open(self.filedirtemp + pair + '.json', 'w') as outfile:
                             json.dump(jsonOrderbookFiltered, outfile)
                     except HTTPError as http_err:
                         print(f'HTTP error occurred: {http_err}')  # Python 3.6
+                        status_code = http_err.response.status_code
+                        if status_code == 429:
+                            print("Pair: " + pair)
                     except Exception as err:
                         print(f'Other error occurred: {err}')  # Python 3.6
-                    else:
-                        print('Success getting pairs!')
+                    #else:
+                        #print('Success getting pairs!')
                 i = i + 1  
             self.calculateCircuit(circuit)
-
+            counter = counter + 1
+            if counter == 20:
+                #print("Sleeping")
+                time.sleep(3)
+                counter = 0
     def calculateCircuit(self, circuit):
         startOrderbook = []
         f = open(self.filedirtemp + circuit[0] + '.json', "r")
@@ -120,9 +130,22 @@ class pairings:
         f = open(self.filedirtemp + circuit[2] + '.json', "r")
         compareOrderbook = json.loads(f.read())
         #console.log((100 / toFixed(buyerE1[0][0])) * toFixed(sellerE2[0][0]) * toFixed(sellerE3[0][0]))
-        arbitrage = 100 / float(startOrderbook[0][0]) * float(tokenOrderbook[0][0]) * compareOrderbook[0][0]
-        print(circuit)
-        print(arbitrage)
+        if len(startOrderbook) > 0 and len(tokenOrderbook) > 0 and len(compareOrderbook) > 0:
+            arbitrage = 100 / float(startOrderbook[0][0]) * float(tokenOrderbook[0][0]) * float(compareOrderbook[0][0])
+            if arbitrage > 105:
+                print(circuit)
+                print(arbitrage)
+                #["tETHUSD", "tETHBTC", "tBTCUSD"]100.01295826377294
+                found = circuit
+                found.append(arbitrage)
+                found.append(time.time())
+                with open("arbitrage.txt", "a") as myfile:
+                    #myfile.write(circuit + ':' + arbitrage + '\n')
+                    json.dump(found, myfile)
+                    myfile.write('\n')
+    def clearTemp(self):
+        [f.unlink() for f in Path(self.filedirtemp).glob("*") if f.is_file()] 
+
 pairing = pairings()
 #pairing.fetchMasterData()
 #print(pairing.getPairings())
@@ -130,5 +153,24 @@ pairing = pairings()
 #pairing.splitMasterdata('USD')
 #pairing.splitMasterdata('EUR')
 #pairing.splitMasterdata('BTC')
-#pairing.findCircuit('EUR', 'BTC')
-pairing.getOrderbooks('EUR', 'BTC')
+
+while True:
+    print("USD->BTC")
+    pairing.findCircuit('USD', 'BTC')
+    pairing.getOrderbooks('USD', 'BTC')
+    pairing.clearTemp()
+
+    print("EUR->BTC")
+    pairing.findCircuit('EUR', 'BTC')
+    pairing.getOrderbooks('EUR', 'BTC')
+    pairing.clearTemp()
+
+    print("USD->ETH")
+    pairing.findCircuit('USD', 'ETH')
+    pairing.getOrderbooks('USD', 'ETH')
+    pairing.clearTemp()
+
+    print("EUR->ETH")
+    pairing.findCircuit('EUR', 'ETH')
+    pairing.getOrderbooks('EUR', 'ETH')
+    pairing.clearTemp()
